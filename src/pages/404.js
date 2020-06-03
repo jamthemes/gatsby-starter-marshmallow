709513a6afb16100404f44161324d84db9f291c1
Maximilian Torggler <maximilian.torggler@tuta.io>
2020-06-03 22:27:58 +0200

    better 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,31 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function NotFound() {
-  return <div>Nothing here...</div>
+  return (
+    <section id="notfound" className="home">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12">
+            <div className="main-banner">
+              <div className="d-sm-flex justify-content-between">
+                <div>
+                  <div className="banner-title">
+                    <h3 className="font-weight-medium">Nothing here :/</h3>
+                  </div>
+                  <p className="mt-3">Let's go back and try again</p>
+                  <Link to="/" className="btn btn-secondary mt-3">
+                    Home
+                  </Link>
+                </div>
+                {/* <div className="mt-5 mt-lg-0">
+                  <img src={group} alt="marsmello" className="img-fluid" />
+                </div> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
